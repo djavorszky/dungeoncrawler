309526671a19e60f53a9ad01b65d9a13689d2b5f
Daniel <daniel.javorszky@gmail.com>
2021-05-07 16:21:03 +0200

    Add map

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+mod map;
+
+mod prelude {
+    pub use crate::map::*;
+    pub use bracket_lib::prelude::*;
+
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+}
+
+use prelude::*;
+
+struct State {
+    map: Map,
+}
+
+impl State {
+    fn new() -> Self {
+        State { map: Map::new() }
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        self.map.render(ctx);
+        ctx.print_color(1, 1, WHITE, BLACK, "Hello terminal!");
+    }
+}
+
+fn main() -> BError {
+    let ctx = BTermBuilder::simple80x50()
+        .with_title("Rusty Dungeon")
+        .with_fps_cap(30.0)
+        .build()?;
+
+    main_loop(ctx, State::new())
 }
---
+++ src/map.rs
@@ -0,0 +1,55 @@
+use crate::prelude::*;
+
+const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+
+#[derive(Copy, Clone, PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor,
+}
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+}
+
+impl Map {
+    pub fn new() -> Self {
+        Self {
+            tiles: vec![TileType::Floor; NUM_TILES],
+        }
+    }
+
+    pub fn render(&self, ctx: &mut BTerm) {
+        for y in 0..SCREEN_HEIGHT {
+            for x in 0..SCREEN_WIDTH {
+                let idx = map_idx(x, y);
+
+                match self.tiles[idx] {
+                    TileType::Wall => ctx.set(x, y, GREEN, BLACK, to_cp437('#')),
+                    TileType::Floor => ctx.set(x, y, YELLOW, BLACK, to_cp437('.')),
+                }
+            }
+        }
+    }
+
+    pub fn in_bounds(&self, point: Point) -> bool {
+        point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
+    }
+
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        self.in_bounds(point) && self.tiles[map_idx_point(point)] == TileType::Floor
+    }
+
+    pub fn try_idx(&self, point: Point) -> Option<usize> {
+        self.in_bounds(point).then(|| map_idx_point(point))
+    }
+}
+
+pub fn map_idx(x: i32, y: i32) -> usize {
+    ((y & SCREEN_WIDTH) + x) as usize
+}
+
+#[inline]
+pub fn map_idx_point(point: Point) -> usize {
+    map_idx(point.x, point.y)
+}
