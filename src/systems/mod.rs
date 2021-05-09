373223191f359925ef30aeb7e3616344859ec077
Daniel <daniel.javorszky@gmail.com>
2021-05-09 15:54:25 +0200

    Make it turn based

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,4 +1,5 @@
 mod collisions;
+mod end_turn;
 mod entity_renderer;
 mod map_render;
 mod player_input;
@@ -6,12 +7,33 @@ mod random_mover;
 
 use crate::prelude::*;
 
-pub fn build_scheduler() -> Schedule {
+pub fn build_input_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
+        .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_renderer::entity_render_system())
+        .build()
+}
+
+pub fn build_player_scheduler() -> Schedule {
+    Schedule::builder()
         .add_system(collisions::collision_system())
+        .flush()
+        .add_system(map_render::map_render_system())
+        .add_system(entity_renderer::entity_render_system())
+        .add_system(end_turn::end_turn_system())
+        .build()
+}
+
+pub fn build_monster_scheduler() -> Schedule {
+    Schedule::builder()
         .add_system(random_mover::random_movement_system())
+        .flush()
+        .add_system(collisions::collision_system())
+        .flush()
+        .add_system(map_render::map_render_system())
+        .add_system(entity_renderer::entity_render_system())
+        .add_system(end_turn::end_turn_system())
         .build()
 }
